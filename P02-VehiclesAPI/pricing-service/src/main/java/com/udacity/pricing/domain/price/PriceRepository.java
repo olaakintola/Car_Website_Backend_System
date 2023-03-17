e71ab429f9cf661213d2a36b4ac6f9aac11d738a
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2023-03-17 11:58:30 +0000

    configure base path for Pricing Service REST APIs

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-
-//@RepositoryRestResource(collectionResourceRel = "services/price", path = "services/price")
 @RepositoryRestResource(collectionResourceRel = "vehicle-price", path = "vehicle-price")
 public interface PriceRepository extends CrudRepository<Price, Long> {
      List<Price> findPriceByVehicleId(@Param("vehicleId") Long vehicleId);
