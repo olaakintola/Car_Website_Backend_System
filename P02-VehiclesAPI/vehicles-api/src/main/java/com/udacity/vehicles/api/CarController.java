140c699e05ea42501cad77e18e7c5d1e27e9797a
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2023-04-17 14:15:09 +0100

    implemented the HATEOAS-driven REST API for Update Operation

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -99,7 +99,9 @@ class CarController {
          * TODO: Use the `assembler` on that updated car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
+        car.setId(id);
+        Car updatedCar = carService.save(car);
+        Resource<Car> resource = assembler.toResource(updatedCar);
         return ResponseEntity.ok(resource);
     }
 
