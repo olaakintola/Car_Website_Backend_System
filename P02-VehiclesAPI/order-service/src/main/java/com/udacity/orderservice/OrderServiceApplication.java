3214cf5de234e23c5cfe959230a951eb953c1feb
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2023-06-08 23:26:57 +0100

    converted the orders api to a microservice registered on eureka server

--- P02-VehiclesAPI/order-service/src/main/java/com/udacity/orderservice/OrderServiceApplication.java
+++ P02-VehiclesAPI/order-service/src/main/java/com/udacity/orderservice/OrderServiceApplication.java
@@ -3,10 +3,12 @@ package com.udacity.orderservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
