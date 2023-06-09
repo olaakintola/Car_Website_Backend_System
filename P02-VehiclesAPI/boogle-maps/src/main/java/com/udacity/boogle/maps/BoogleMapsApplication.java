9b355035f2b71d0651a9422efe07768c7a772a82
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2023-06-09 14:36:54 +0100

    implemented the api documentation for the maps microsvervice

--- P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/BoogleMapsApplication.java
+++ P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/BoogleMapsApplication.java
@@ -1,11 +1,18 @@
 package com.udacity.boogle.maps;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info( title = "Maps API", version = "1.0", description = "This is a Maps API server created using springdocs - a library for OpenAPI 3 with spring boot.",
+		license = @License(name = "Apache 2.0", url = "http://foo.bar"),
+		contact = @Contact(url = "https://ie.linkedin.com/in/olanipekun-akintola-6746731b7", name = "OllaOla", email = "ola@gmail.com") ) )
 public class BoogleMapsApplication {
 
 	public static void main(String[] args) {
